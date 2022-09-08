f099c6868f3118fd13e0a12b22a73ecd6b134963
jangjeeyu <112834585+jangjeeyu@users.noreply.github.com>
2022-09-08 16:35:32 +0900

    220908

--- js/center.js
+++ js/center.js
@@ -14,10 +14,31 @@ $(function(){
 
 //식단안내 슬릭
 $(function(){
-  $(".today_meal .regular").slick({
+  $(".today_meal .meal_slider").slick({
     dots: false,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    centerMode:true
   });
+})
+
+//양한방 프로그램 슬릭
+$(document).ready(function(){
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: false,
+    focusOnSelect: true
+  });
+      
 })
