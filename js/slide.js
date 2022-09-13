db33c7f2e0e050da1b78e36fb8be1d1ac0328706
jangjeeyu <112834585+jangjeeyu@users.noreply.github.com>
2022-09-13 09:06:03 +0900

    0913

---
+++ js/slide.js
@@ -0,0 +1,60 @@
+//메인
+$(function(){
+  $(".lazy").slick({
+      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      infinite: true,
+      arrows:false,
+      dots:true
+    });
+})
+
+//간호간병 서비스 슬릭
+$(function(){
+    $(".center").slick({
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed: 1500,
+        pauseOnHover : true,
+      });
+})
+
+//양한방 프로그램 슬릭
+$(function(){
+  $("#sec4_prog1 .slider-for ").slick({
+    lazyLoad: 'ondemand',
+    infinite: true, 
+    arrows : true,
+    asNavFor: '#sec4_prog1  .slider-nav'
+  }); 
+  $("#sec4_prog1  .slider-nav").slick({
+    lazyLoad: 'ondemand', 
+    infinite: true, 
+    arrows : false,
+    fade: true,
+    asNavFor: '#sec4_prog1  .slider-for'
+  }); 
+})
+//-----------------재활--------------------- 
+$(function(){
+  $(".sec5_slider").slick({
+      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      infinite: true,
+      arrows:false,
+      dots:true,
+      autoplay:true
+    });
+})
+//식단안내 슬릭
+$(function(){
+  $(".today_meal .meal_slider").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode:true
+  });
+})
--- js/user.js
+++ js/user.js
@@ -5,4 +5,14 @@ $(function(){
     })
   })
 
+//탑버튼
+$(function(){
+    $(".top").click(function(){
+        $('html, body').animate({
+        scrollTop : 0
+        }, 800);
+        return false;
+        });
+})
+
   
