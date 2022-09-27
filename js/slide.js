c06d6abeb786b85cce4c879a2c561d1b82478a5c
jangjeeyu <112834585+jangjeeyu@users.noreply.github.com>
2022-09-27 17:23:20 +0900

    0927

--- js/slide.js
+++ js/slide.js
@@ -5,68 +5,59 @@ $(function(){
       infinite: true,
       arrows:false,
       dots:true,
-      autoplay:true
+      autoplay:false
     });
-})
 
-//간호간병 서비스 슬릭
-$(function(){
+    //간호간병 서비스 슬릭
     $(".center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed: 1500,
-        pauseOnHover : true,
-      });
-})
-
-//양한방 프로그램 슬릭
-$(function(){
-  $("#sec4_prog1 .slider-for ").slick({
-    lazyLoad: 'ondemand',
-    infinite: true, 
-    arrows : true,
-    asNavFor: '#sec4_prog1  .slider-nav'
-  }); 
-  $("#sec4_prog1  .slider-nav").slick({
-    lazyLoad: 'ondemand', 
-    infinite: true, 
-    arrows : false,
-    fade: true,
-    asNavFor: '#sec4_prog1  .slider-for'
-  }); 
-})
-//-----------------재활--------------------- 
-$(function(){
-  $(".sec5_slider").slick({
-      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      dots: false,
       infinite: true,
-      arrows:false,
-      dots:false,
-      autoplay:true
+      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay:false,
+      autoplaySpeed: 1500,
+      pauseOnHover : true,
+      responsive: [                   
+        {  breakpoint: 800,    
+               settings: {
+                 slidesToShow: 3,
+                 slidesToScroll: 3
+               }
+          }
+  ]  //반응형 포인트 지정
     });
-})
+
+    //양한방 프로그램 슬릭
+    $("#sec4_prog1 .slider-for ").slick({
+      lazyLoad: 'ondemand',
+      arrows : false,
+      infinite: true, 
+      asNavFor: '#sec4_prog1  .slider-nav'
+    }); 
+    $("#sec4_prog1  .slider-nav").slick({
+      lazyLoad: 'ondemand', 
+      infinite: true, 
+      arrows : true,
+      fade: true,
+      asNavFor: '#sec4_prog1  .slider-for'
+    }); 
+
 //식단안내 슬릭
-$(function(){
-  $(".today_meal .meal_slider").slick({
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode:true
-  });
-})
+$(".today_meal .meal_slider").slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode:true
+});
 
 //퀵버튼-상담예약 팝업창
-$(function(){
-  $('#call_quick .main').click(function(){
-    $('.call_box').show()
-  });
-  // $('.modal').click(function(){
-  //   $('#pop_up').fadeIn(600);
-  //   winOpen()
-  // })
+$('#call_quick .main').click(function(){
+  $('.call_box').show()
+});
+    // $('.modal').click(function(){
+    //   $('#pop_up').fadeIn(600);
+    //   winOpen()
+    // })
 })
--- js/user.js
+++ js/user.js
@@ -3,60 +3,60 @@ $(function(){
     $('.lang').click(function(){
       $('.div1').slideToggle();
     })
-  })
-
-// 네비 버튼
-$(function(){
+  
+  // 네비 버튼
+  $('.m_sub').hide();
   $('.close1').click(function(){
-    $('.burg_nav').animate({right:'-100%'});
-  });
+       $('.burg_nav').animate({right:'-100%'});
+     });
   $('.burg_btn > img').click(function(){
-    $('.burg_nav').animate({right:'0'});
-  })
+      $('.burg_nav').animate({right:'0'});
+     })
   $('#btn1>li').each(function(){
     var depth1_a = $(this).children('a');
     var depth2_sub = $(this).children('.m_sub');
     var sub_status = 0; /*0--닫힘 1--열림*/
+    depth1_a.click(function(){ 
+      $('.m_sub').hide();
+      depth2_sub.toggle();
 
-    depth1_a.click(function(){
+          /*
       if(sub_status == 0){
-
         $('.m_sub').hide();
         $('#btn1>li>a').removeClass('active');
-        sub_status = 0;
 
         depth2_sub.show();
         $(this).addClass('active');
         sub_status = 1;
-
       }else {
         depth2_sub.hide();
         $(this).removeClass('active');
         sub_status = 0;
-      }
-    })
-  })
-})
+      }  */
+
+
+    })  //depth1_a.click
+
+
+   
+
+  })   //  $('#btn1>li').each
 
 //탑버튼
-$(function(){
     $(".top").click(function(){
         $('html, body').animate({
         scrollTop : 0
         }, 800);
         return false;
         });
-})
 
 //상담 팝업
-$(function(){
   $('.call_box .close').click(function(){
     $('.call_box').fadeOut(400)
   });
   $('.main').click(function(){
     $('.call_box').fadeIn(400);
   })
-})
 
-//전화 퀵버튼
+})
   
